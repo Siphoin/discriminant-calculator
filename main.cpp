dd291d88ff2d8bd54960ec40c22a54e1b95f6794
Максим Косолапов <maksimka.kosolapov.01@mail.ru>
2020-09-22 18:58:32 +0500

    7 commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,6 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-bool is_number(const std::string& s)
-{
-	std::string::const_iterator it = s.begin();
-	while (it != s.end() && isdigit(*it)) ++it;
-	return !s.empty() && it == s.end();
-}
-
 
 void main() {
 	system("chcp 65001");
@@ -15,50 +8,27 @@ void main() {
 	cout << "Нахождение Дискриминанта" << endl;
 	while (true)
 	{
-int a = 0;
-	int b = 0;
-	int c = 0;
+		int a = 0;
+		int b = 0;
+		int c = 0;
+		cout << "Введите переменные: a, b, с" << endl;
+		cin >> a >> b >> c;
+		cout << "Ваши данные:\na = " << a << "\nb = " << b << "\nc = " << c << endl;
 
-	string a_str = "";
-	string b_str = "";
-	string c_str = "";
-	
-	cout << "Введите переменные: a, b, с" << endl;
-	cin >> a_str >> b_str >> c_str;
+		float result = pow(b, 2) - 4 * a * c;
+		if (result <= 0) {
+			cout << "Нет вещественных корней." << endl;
+			cin;
+		}
 
-	if (!is_number(a_str)) {
-		cout << "Ошибка" << endl;
-		cin;
-		return;
+		else {
+			float d = sqrt(result);
+			float x1 = (b * -1 + d) / pow(a, 2);
+			float x2 = (b * -1 - d) / pow(a, 2);
+			cout << "\nD = " << d << endl;
+			cout << "\nX1 = " << x1 << endl;
+			cout << "\nX2 = " << x2 << endl;
+
+		}
 	}
-	if (!is_number(b_str)) {
-		cout << "Ошибка" << endl;
-		cin;
-		return;
-	}
-
-	if (!is_number(c_str)) {
-		cout << "Ошибка" << endl;
-		cin;
-		return;
-	}
-
-	a = atol(a_str.c_str());
-	b = atol(b_str.c_str());
-	c = atol(c_str.c_str());
-	cout << "Ваши данные:\na = " << a << "\nb = " << b << "\nc = " << c << endl;
-
-	float result = pow(b, 2) - 4 * a * c;
-	if (result <= 0) {
-		cout << "Нет вещественных корней." << endl;
-		cin;
-	}
-
-	else {
-	float d = sqrt(result);
-	cout << "\nD = " << d << endl;
-	}
-
-	}
-	
- }
+}
