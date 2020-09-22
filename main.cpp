a75a86498aa21f10b9ea5ec3f71c2effce9403e8
Максим Косолапов <maksimka.kosolapov.01@mail.ru>
2020-09-22 10:34:01 +0500

    Добавьте файлы проекта.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+void main() {
+	setlocale(LC_ALL, "RU");
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	cout << " Нахождение Дискриминанта" << endl;
+	cout << " Введите переменные: a, b, с" << endl;
+	cin >> a >> b >> c;
+	if (pow(b, 2) - 4 * a * c <= 0) {
+		cout << "Невозможно взять числа т.к d <= 0" << endl;
+		cin;
+		return;
+	}
+	float d = sqrt(pow(b, 2) - 4 * a * c);
+	cout << "D = " << d << endl;
+	cin;
+ }
