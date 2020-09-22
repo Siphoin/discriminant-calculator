b787ef4aba6fb764a467a66985907323153775e9
Максим Косолапов <maksimka.kosolapov.01@mail.ru>
2020-09-22 11:20:03 +0500

    4 commit

--- main.cpp
+++ main.cpp
@@ -4,20 +4,26 @@ using namespace std;
 
 void main() {
 	system("chcp 65001");
-	int a = 0;
+	system("cls");
+	cout << "Нахождение Дискриминанта" << endl;
+	while (true)
+	{
+int a = 0;
 	int b = 0;
 	int c = 0;
-	setlocale(LC_ALL, "Ru");
-	cout << "Нахождение Дискриминанта" << endl;
 	cout << "Введите переменные: a, b, с" << endl;
 	cin >> a >> b >> c;
 	float result = pow(b, 2) - 4 * a * c;
 	if (result <= 0) {
 		cout << "Нет вещественных корней." << endl;
 		cin;
-		return;
 	}
+
+	else {
 	float d = sqrt(result);
 	cout << "D = " << d << endl;
-	cin;
+	}
+
+	}
+	
  }
